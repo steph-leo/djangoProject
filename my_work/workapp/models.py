1eac9ebfdb8d97ce5b4ca33eba8bceaa9a481e03
steph-leo <leostephs89@gmail.com>
2025-01-18 13:16:28 +0300

    Add folder and files

--- my_work/workapp/models.py
+++ my_work/workapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Workapp(models.Model):
+    firstname = models.CharField(max_length = 255)
+    lastname = models.CharField(max_length = 255)
+    mobilephone = models.IntegerField(null = True)
+    startDate = models.DateField(null = True)
--- my_work/workapp/views.py
+++ my_work/workapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Workapp
+# Create your views here
+def workapp(request):
+    info = Workapp.objects.all().values()
+    template = loader.get_template("index.html")
+    context = {
+        'info': info ,
+    }
+    return HttpResponse(template.render(request, context))
 
-# Create your views here.
+def details(request, id):
+    info = Workapp.objects.get(id=id)
+    template = loader.get_template("details.html")
+    context = {
+        'info' : info,
+    }
+    return HttpResponse(template.randeer(context, request))
